8087506a822ed678dcebc452d5db12bd7a6edcd2
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2024-03-24 15:53:53 +0100

    ashura
    adding verification pwd

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -19,8 +19,10 @@ function validateForm()
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPrenom);
     const mailOk = checkmail(inputMail);
+    const passwordOk = checkPwd(inputPassword);
+    const validPwdOk = validedPwd(inputPassword, inputValidPassowrd);
     checkmail(inputMail);
-    if (nomOk && prenomOk && mailOk) {
+    if (nomOk && prenomOk && mailOk && passwordOk && validPwdOk) {
         btnValidation.disabled = false;
     } else {
         btnValidation.disabled = true;
@@ -33,6 +35,38 @@ function checkmail(input)
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const mailUser = input.value;
     if (mailUser.match(emailRegex)){
+         //ça passe, c'est ok
+         input.classList.add("is-valid");
+         input.classList.remove("is-invalid");
+         return true;
+    }else{
+        //ça passe pas, c'est pas ok
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+} 
+
+function validedPwd(inputpwd, inputvalidpwd) 
+{
+    if (inputpwd.value == inputvalidpwd.value) {
+        inputvalidpwd.classList.add("is-valid");
+        inputvalidpwd.classList.remove("is-invalid");
+        return true;  
+    } else{
+        //ça passe pas, c'est pas ok
+        inputvalidpwd.classList.remove("is-valid");
+        inputvalidpwd.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function checkPwd(input)
+{
+    // définir mon expression régulière 
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const PwdUser = input.value;
+    if (PwdUser.match(passwordRegex)){
          //ça passe, c'est ok
          input.classList.add("is-valid");
          input.classList.remove("is-invalid");
