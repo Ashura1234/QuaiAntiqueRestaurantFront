29fe1fc7dab181ae0c0482a7b03db83c12fadea3
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2024-03-25 16:55:29 +0100

    ashura
    [EDIT] system of connexion and right for user

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, autoRize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.autoRize = autoRize;
     }
 }
+
+/*
+[] -> tout le monde peut y accéder
+["disconnected"] -> réserver aux utilisateur déconnected
+["client"] -> réserver aux utilisateurs avec le rôle client 
+["admin"] -> réserver aux utilisateurs avec le rôle admin 
+["client", "admin"] -> réserver aux utilisateurx avec le rôle client ou le rôle admin 
+
+*/
