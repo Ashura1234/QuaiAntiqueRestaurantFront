f922a819b886f35a64ee89ee0a3ba916e24eb639
Ashura1234 <156065272+Ashura1234@users.noreply.github.com>
2024-03-26 17:08:15 +0100

    Ashura
    [ADD CODE] function verify fail XSS

---
+++ js/galerie.js
@@ -0,0 +1,25 @@
+const galerieImage = document.getElementById("allImage");
+
+//Récupérer les informations des images
+let titre = '<img src=x onerror="window.location.replace(\'https://google.com\')"/>'
+let imageSource = "../image/table.jpg"
+let monImage = getImage(titre, imageSource);
+
+galerieImage.innerHTML = monImage;
+
+
+function getImage(titre, urlImage)
+{
+    titre = SanitizeHtml(titre);
+    urlImage = SanitizeHtml(urlImage);
+    return `<div class="col p-3">
+                <div class="image-card text-white">
+                    <img src="${urlImage}" class="rounded w-100"/>
+                    <p class="titre-image">${titre}</p>
+                    <div class="action-image-buttons">
+                        <button data-show="admin" type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#EditionPhotoModal"><i class="bi bi-pencil-square"></i></button>
+                        <button data-show="admin" type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#DeletePhotoModal"><i class="bi bi-trash"></i></button>
+                    </div>
+                </div>
+            </div>`;
+}
